fe02e978aff431f8c11a4d55fcedc461602822f6
anton <antonbezr@gmail.com>
2020-07-17 13:36:47 -0700

    u

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ document.getElementById('button').addEventListener('click', get);
 
 function get(){
   var xhr = new XMLHttpRequest();
-  // xhr.open('GET', 'sudoku.php?var=easy,100', true);
   xhr.open('GET', 'https://frozen-lake-90320.herokuapp.com/sudoku.php?var=easy,100', true);
   xhr.onload = function(){
   	document.getElementById('text').innerHTML = this.responseText;
